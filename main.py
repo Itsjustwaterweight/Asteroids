4c2ee6fb4c2ea779deda58ca20346e23f46a9b85
Itsjustwaterweight <Isaiahsmith05@gmail.com>
2026-03-06 14:54:13 -0500

    Implemeted delta time

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
         
         screen.fill("black")
         pygame.display.flip()
+
+        dt = fps.tick(60)
+
+        dt = dt / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
