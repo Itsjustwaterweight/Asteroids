6eb18c798093dafe63fe02c2ac8a0837910b2dc8
Itsjustwaterweight <Isaiahsmith05@gmail.com>
2026-04-04 19:32:27 -0400

    Shooting enabled

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from asteroidfield import AsteroidField
 from asteroid import Asteroid
 from logger import log_event
 from circleshape import *
+from shot import Shot
 
 
 def main():
@@ -18,10 +19,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
     asteroid_field = AsteroidField()
